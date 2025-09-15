ddad89417522f64411090cc56c6a5dfd6c276c32
Raymond Mukonda <RaymondKogen@gmail.com>
2025-09-15 12:08:56 +0200

    created an is valid function

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -19,31 +19,44 @@ class Program
 
 
 
-        Console.Write("Would you like to keep playing? ");
-        string response = Console.ReadLine();
+    string userInput = "";
 
-        while (response == "yes")
-        {
-            Console.Write("What is the magic number(1-10): ");
-            string userInput = Console.ReadLine();
-            int input = int.Parse(userInput);
+    while (userInput != "end")
+{
+    Console.Write("Guess the magic number (1-10) or type 'end' to quit: ");
+    userInput = Console.ReadLine().ToLower();
 
-            Random randomGenerator = new Random();
-            int computerNumber = randomGenerator.Next(1, 11);
+    if (userInput == "end")
+    {
+        Console.WriteLine("Thanks for playing!");
+        break;
+    }
 
-            if (input == computerNumber)
-            {
-                Console.WriteLine("You guessed it right!");
-            }
-            else if (input < computerNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("Higer");
-            }
-        }
+    int input;
+    bool isValid = int.TryParse(userInput, out input);
+
+    if (!isValid || input < 1 || input > 10)
+    {
+        Console.WriteLine("Please enter a valid number between 1 and 10.");
+        continue;
+    }
+
+    Random randomGenerator = new Random();
+    int computerNumber = randomGenerator.Next(1, 11);
+
+    if (input == computerNumber)
+    {
+        Console.WriteLine("You guessed it right!");
+    }
+    else if (input < computerNumber)
+    {
+        Console.WriteLine("Higher");
+    }
+    else
+    {
+        Console.WriteLine("Lower");
+    }
+}
 
 
 
